03305cbe5d4d255e9499235e35b2350976a0db2e
Andy Hinchliffe <118820952+andyhinchliffe@users.noreply.github.com>
2023-11-22 10:19:35 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const time = document.querySelector(`.clock`);
+
+const tick = ()=> {
+
+    const now = new Date();
+
+    
+    
+
+
+
+    let h = now.getHours();
+
+    
+if (h > 12) {
+    h -=12;
+} else if (h === 0) {
+   h = 12;
+}
+
+console.log(h)
+
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+
+    const html = `
+    <span> ${h}</span> : 
+    <span> ${m}</span> : 
+    <span> ${s}</span>
+
+    `;
+
+   
+time.innerHTML = html;
+};
+
+setInterval(tick, 1000);
